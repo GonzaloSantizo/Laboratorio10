04182a9963bef82aac333cb80edfc598090abd7d
GonzaloSantizo <san21504@uvg.edu.gt>
2022-09-22 08:22:02 -0600

    Second changes

--- app/src/main/java/com/durini/solucionlab8y9/ui/activities/MainActivity.kt
+++ app/src/main/java/com/durini/solucionlab8y9/ui/activities/MainActivity.kt
@@ -3,6 +3,9 @@ package com.durini.solucionlab8y9.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.durini.solucionlab8y9.R
+import android.content.Intent
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
 
